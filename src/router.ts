686f1afb2586aa9f3eb41ad1741f68d8a0c87444
zhao xin ke <zhaoxinke@boco.com.cn>
2018-10-12 16:31:46 +0800

    添加prettier配置,测试路由

--- src/router.ts
+++ src/router.ts
@@ -5,9 +5,10 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home,
     },
@@ -17,7 +18,15 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue'),
+      children: [
+        {
+          path: 'hello',
+          component: () =>
+            import(/* webpackChunkName: "about" */ './views/About.vue'),
+        },
+      ],
     },
   ],
 });
