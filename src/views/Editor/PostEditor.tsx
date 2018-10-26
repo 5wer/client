d53261796d38470ac39daf83b0f76af00da7fd11
Xinke Zhao <felixzxk@gmail.com>
2018-10-26 10:48:13 +0800

    集成tinymce

--- src/views/Editor/PostEditor.tsx
+++ src/views/Editor/PostEditor.tsx
@@ -1,14 +1,44 @@
 import { Vue, Component } from 'vue-property-decorator';
+import tinymce from 'tinymce';
+import 'tinymce/themes/modern/theme';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/contextmenu';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/colorpicker';
+import 'tinymce/plugins/textcolor';
+import Editor from '@tinymce/tinymce-vue';
 
 @Component({
-  components: {},
+  components: { Editor },
 })
 export default class PostEditor extends Vue {
-  mounted() {}
+  tinymceHtml: string = '请输入内容';
+  init = {
+    language_url: '/tinymce/zh_CN.js',
+    language: 'zh_CN',
+    skin_url: '/tinymce/skins/lightgray',
+    height: 300,
+    plugins: 'link lists image code table colorpicker textcolor wordcount contextmenu',
+    toolbar: `bold italic underline strikethrough |
+      fontsizeselect | forecolor backcolor |
+      alignleft aligncenter alignright alignjustify |
+      bullist numlist |
+      outdent indent blockquote |
+      undo redo |
+      link unlink image code |
+      removeformat`,
+    branding: false,
+  };
+  mounted() {
+    tinymce.init({});
+  }
   check() {
     const selection = window.getSelection();
     const range = selection.toString() && selection.getRangeAt(0);
-    console.log(selection, range);
   }
   render() {
     return (
@@ -17,17 +47,7 @@ export default class PostEditor extends Vue {
         style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
         id="richtext"
       >
-        <el-input />
-        <el-button-group style={{ margin: '10px auto' }}>
-          <el-button onClick={this.check} icon="el-icon-edit" />
-          <el-button icon="el-icon-save" />
-          <el-button icon="el-icon-delete" />
-        </el-button-group>
-        <div
-          style={{ flex: 1, padding: '10px', overflow: 'auto' }}
-          contenteditable
-          value="hello world"
-        />
+        <editor id="tinymce" v-model={this.tinymceHtml} init={this.init} />
       </div>
     );
   }
